bc571fee8bd333a9db40e0f8aeab33933c51d448
IT-WIBRC <wibrc.se.jc@gmail.com>
2024-10-07 16:58:28 +0100

    [Task] | Init project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { URL, fileURLToPath } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    },
+    build: {
+      outDir: mode === "staging" ? "dist-staging" : "dist"
+    }
+  };
+});
