28112b46cd51dc38857de86e47779f72cbda6d5d
IT-WIBRC <wibrc.se.jc@gmail.com>
2024-11-05 18:40:01 +0100

    [Task-#8696c8qrh] | Update the project structure

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,12 @@ export default defineConfig(({ mode }) => {
     plugins: [vue(), vueDevTools()],
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url))
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@core": fileURLToPath(new URL("./src/core", import.meta.url)),
+        "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
+        "@utils": fileURLToPath(new URL("./src/utils", import.meta.url)),
+        "@services": fileURLToPath(new URL("./src/services", import.meta.url)),
+        "@assets": fileURLToPath(new URL("./src/assets", import.meta.url))
       }
     },
     build: {
