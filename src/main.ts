136461c91e4c741f9db9a1cbae9304fff91bf6ce
IT-WIBRC <wibrc.se.jc@gmail.com>
2024-11-05 18:40:01 +0100

    [Task-#8696c8qrh] | Update the project structure

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import "./assets/main.css";
+import "@assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { setApiBaseUrl } from "@utils/apiConfig";
+import { createI18n } from "vue-i18n";
+import i18nMessages from "@modules/i18n.json";
 
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
+app.use(router).use(createI18n({
+  locale: navigator.language.split("-")[0],
+  legacy: false,
+  fallbackFormat: "fr",
+  messages: i18nMessages,
+}));
 
 app.mount("#app");
+setApiBaseUrl();
