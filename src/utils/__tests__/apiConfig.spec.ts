4e62cdd7e565092abd2b11062ecdd882f5e62dea
IT-WIBRC <wibrc.se.jc@gmail.com>
2025-05-02 18:29:42 +0100

    fix(test): vitest doesn't read env variables on test
    closes #33

--- src/utils/__tests__/apiConfig.spec.ts
+++ src/utils/__tests__/apiConfig.spec.ts
@@ -4,9 +4,7 @@ import { OpenAPI } from "@/services/apis/user";
 
 describe("apiConfig", () => {
   it("should set the config", () => {
-    const apiUrl = "http://localhost:5173/api";
-    process.env.VITE_API_URL = apiUrl;
     setApiBaseUrl();
-    expect(OpenAPI.BASE).toBe(apiUrl);
+    expect(OpenAPI.BASE).toBe(import.meta.env.VITE_API_URL);
   });
 });
